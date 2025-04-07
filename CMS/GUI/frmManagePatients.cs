c9935d2eb5ec5949fd568d839d50efd660195860
AlanHo <alanhoitcd@gmai.com>
2025-04-07 17:05:07 +0700

    update

--- CMS/GUI/frmManagePatients.cs
+++ CMS/GUI/frmManagePatients.cs
@@ -48,14 +48,6 @@ namespace CMS.GUI
             if (parentTab != null && tabControl != null)
             {
                 tabControl.TabPages.Remove(parentTab); // Xóa tab khỏi TabControl nhưng không dispose
-                if (tabControl.TabPages.Count == 0)
-                {
-                    tabControl.Hide();
-                }
-                else
-                {
-                    tabControl.Show();
-                }
             }
         }
     }
